ed62aea38572e15e76a9344ce79d5da6416ffbcd
Joao Hinning <jphinning@gmail.com>
2022-07-19 00:59:42 -0300

    fixup! fixup! fixup! fixup! feat:Development finished

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -5,6 +5,8 @@ import { DataSource } from 'typeorm';
 import { ConfigurationModule } from './config/config.module';
 import { FormsModule } from './forms/forms.module';
 
+console.log(process.env.DB_USER)
+
 @Module({
   imports: [ConfigurationModule, FormsModule],
   controllers: [],
